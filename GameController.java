1e2fe01d357610ffe6c92177c7063bde94b37526
KesVisser <92203793+KesVisser@users.noreply.github.com>
2021-10-16 14:16:35 +0200

    Add files via upload

---
+++ GameController.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ * 
+ * @author 
+ *
+ */
+public class GameController {
+	
+	//Global Variables
+	private GameBoard game;
+	
+	
+	/**
+	 * 
+	 * @param inital
+	 */
+	public GameController(GameBoard inital) {
+		
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public GameBoard getBoard() {
+		return game;
+	}
+	/**
+	 * 
+	 */
+	public void createNewGame() {
+		
+	}
+	/**
+	 * 
+	 */
+	public void moveTile() {
+		
+	}
+	/**
+	 * 
+	 */
+	public void clearBoard() {
+		
+	}
+	
+
+}
