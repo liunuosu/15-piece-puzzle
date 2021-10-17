e8f66da2dea17d9651d817859c48c3cf08f7d069
KesVisser <92203793+KesVisser@users.noreply.github.com>
2021-10-17 14:23:33 +0200

    GameControl with comments

--- GameController.java
+++ GameController.java
@@ -2,9 +2,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * 
- * 
- * @author 
+ * Class that controls and performs simulation steps.
+ * @author Kes Visser 524858kv
  *
  */
 public class GameController {
@@ -13,58 +12,57 @@ public class GameController {
 	private GameBoard game;
 	private ArrayList<Integer> puzzle = new ArrayList<Integer>();
 	private String solver;
-	private int numberOfSteps;
+	private int numberOfSteps; //number of steps we have made to solve the puzzle
 	private ArrayList<ArrayList<Integer>> moves = new ArrayList<ArrayList<Integer>>();
 	
-	
 	/**
-	 * 
-	 * @param inital
+	 * Constructor which creates a simulation based on a current game board and a solver.
+	 * @param inital The game board to perform a simulation with.
+	 * @param solver the solver with which we which to solve the 15-puzzle.
 	 */
-	// change solve it in different method
 	public GameController(GameBoard inital, String solver) {
-		game = inital;
-		puzzle = inital.getBoard();
+		this.game = inital;
+		this.puzzle = inital.getBoard();
 		this.solver = solver;
-		numberOfSteps = 0;
-		//moves = solver();
+		this.numberOfSteps = 0;
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Getter for obtaining the current 15-puzzle from the game board.
+	 * @return an arrayList that holds integers 0 to 16, which represent our puzzle.
 	 */
 	public GameBoard getBoard() {
 		return game;
 	}
 	
 	/**
-	 * 
+	 * Creates a new 15-puzzle and change the puzzle in game board to this new puzzle. It also resets the numberOfSteps to 0.
 	 */
 	public void createNewGame() {
-		//System.out.println("createNewGame");
-		Puzzle newPuzzle = new Puzzle();
-		
 		puzzle.clear();
-		//ArrayList<Integer> newPuzzle2 = new ArrayList<>((newPuzzle.getBoard()));
-		this.puzzle = newPuzzle.getPuzzle();
-		game.setPuzzle(puzzle); //problem
 		this.numberOfSteps = 0;
-		//moves = solver();
-		//System.out.println("createNewGame2");
+		Puzzle newPuzzle = new Puzzle();
+		this.puzzle = newPuzzle.getPuzzle();
+		game.setPuzzle(puzzle);
 	}
 	
+	/**
+	 * Sets the solver to the user specified type. This can be A* method or the HumanAlgorithm.
+	 * @param solver a String that says A* or HumanAlgorithm.
+	 */
 	public void setSolver(String solver) {
 		this.solver = solver;
 	}
 	
-	public int numberOfSteps() {
-		return moves.size();
-	}
-	
-	//Make a solver class and return moves
+	/**
+	 * This method solves the puzzle and then puts all the moves needed to get there in 'moves'.
+	 * The method checks what type of solver the user wants to use and then calls this class to solve the puzzle. Then it will add all these
+	 * moves equal to 'moves'. A move is a arrayList where 2 numbers from the previous puzzle have switched. See the solvers to see what move
+	 * has been made or why.
+	 */
 	public void solver(){
 		ArrayList<ArrayList<Integer>> moves = new ArrayList<ArrayList<Integer>>();
+		
 		if (solver.equals("A* solver")) {
 			Node initialNode = new Node(game.getBoard(),0);
 			A_star a = new A_star(initialNode);
@@ -72,53 +70,35 @@ public class GameController {
 			kes = a.getPathSet().get(2).getBoard();
 			moves.add(kes);
 			for (int i=a.pathSet.size()-1; i>=0; i--) {
-				//ArrayList<Integer> kes = new ArrayList<Integer>();
-				//kes = a.getPathSet().get(i).getBoard();
-				//moves.add(kes);
+				ArrayList<Integer> sub = new ArrayList<Integer>();
+				sub = a.getPathSet().get(i).getBoard();
+				moves.add(sub);
 			}
 		}
+		
 		else {
 			HumanAlgorithm person = new HumanAlgorithm(game);
 			person.solve();
 			moves = person.getMoves();
 		}
-		//System.out.println(moves);
 		this.moves = moves;
 	}
 	
 	/**
-	 * 
+	 * Method that returns the number of steps need to go from our puzzle to the solved puzzle.
+	 * @return the number of steps needed to get to the solved puzzle.
 	 */
-	//for A_star pathSet contains all the moves
-	//for might mess with the solvers if we use GameBoard, if it does just introduce private puzzle and coppy it in there.
-	public void moveTile() {
-		
-		//game.setPuzzle(moves.get(numberOfMoves));
-		if (game.getBoard().equals(moves.get(1))) {
-			//game.setPuzzle(moves.get(3));
-		}
-		else {
-			//game.setPuzzle(moves.get(1));
-		}
-		game.setPuzzle(moves.get(numberOfSteps));
-		numberOfSteps++;
-		//System.out.println(numberOfMoves);
-		;
-		
+	public int numberOfTotalSteps() {
+		return moves.size();
 	}
 	
 	/**
-	 * 
+	 * Moves a tile in the game. It goes into moves and gets the next puzzle. So the next step according to our solver.
+	 * After having found our new puzzle it sets the puzzle in our game board to this puzzle and it updates number of steps.
 	 */
-	//Maybe we have to call a new gamecontroller? Or wipe moves but no way to get new ones unless we solve the things again
-	public void clearBoard() {
-		Integer[] finishedArray = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0};
-		ArrayList<Integer> finishedPuzzle = new ArrayList<>(Arrays.asList(finishedArray));
-		GameBoard finishedGame = new GameBoard(finishedPuzzle);
-		this.puzzle = finishedPuzzle;
-		this.game = finishedGame;
-		
+	public void moveTile() {
+		game.setPuzzle(moves.get(numberOfSteps));
+		numberOfSteps++;
 	}
-	
 
 }
